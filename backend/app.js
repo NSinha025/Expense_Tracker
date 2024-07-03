ec67f513a1f64422cd250e32ca4890000c8dc621
Naman Sinha <sinhanaman025@gmail.com>
2024-07-03 22:18:36 +0530

    final

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,9 @@ const PORT = process.env.PORT
 //middlewares
 app.use(express.json())
 app.use(cors())
+app.get('/',(req, res)=>{
+    res.send("Hello from backend");
+})
 
 readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -12,4 +12,5 @@ router.post('/add-income', addIncome)
 
 
 
+
 module.exports = router
