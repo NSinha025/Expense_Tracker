97da550ce13fc9da84d9baea3cfffe44a1d22c52
Naman Sinha <sinhanaman025@gmail.com>
2024-07-03 20:38:15 +0530

    deployed

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = "http://localhost:8000/api/v1/";
+const BASE_URL = "https://expense-tracker-jbak.onrender.com";
 
 
 const GlobalContext = React.createContext()
